99c252718d0f809fd8ea632925e33e32e441a2b3
Skeezinho <viktorabemarek@gmail.com>
2026-01-16 19:30:34 +0100

    fix images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -53,7 +53,7 @@ app.use(cors({
   origin: config.corsOrigin,
   credentials: true,
 }));
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '25mb' }));
 app.use('/uploads', express.static(path.resolve('uploads')));
 
 // REST Routes
